d419f53827b8da16e8a375ecace4272c0f9811c3
navpreet <navpreetbatth2.nb@gmail.com>
2024-03-05 13:31:06 -0800

    initial commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // create an object/instance
+            Car myCar = new Car(); // any name instaed of myCar, Car(first one) is a type, can be used var for inferrred type
+            myCar.Make = "Toyota";
+            myCar.Year = 2022;
+            myCar.Model = "Camry";
+
+            // if want to pass just one value , don't need to use $ means(string interpolation)
+            Console.WriteLine($"{myCar.Model} {myCar.Year} {myCar.Model}");
+            
         }
     }
 }
